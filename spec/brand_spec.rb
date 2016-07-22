d5a14e793602bfb680034a8c00d1a48a92432c6a
Matt Carlson <matthewthomascarlson@gmail.com>
2016-07-22 09:27:21 -0700

    add validations and callbacks to Brand and Shoe classes with passing specs

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -8,4 +8,16 @@ describe(Brand) do
     brand.stores.push(store1, store2)
     expect(brand.stores()).to(eq([store1, store2]))
   end
+
+  it("ensures the name is at least one character") do
+    brand = Brand.new({name: ''})
+    expect(brand.save()).to(eq(false))
+  end
+
+  describe('#capitalize_name') do
+    it('capitalizes the name of a brand') do
+      brand = Brand.create({name: 'smerv'})
+      expect(brand.name()).to(eq('Smerv'))
+    end
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -8,4 +8,16 @@ describe(Store) do
     test_store.brands.push(brand1, brand2)
     expect(test_store.brands()).to(eq([brand1, brand2]))
   end
+
+  it("ensures the name is at least one character") do
+    store = Store.new({name: ''})
+    expect(store.save()).to(eq(false))
+  end
+
+  describe('#capitalize_name') do
+    it('capitalizes the name of a brand') do
+      store = Store.create({name: 'buckys'})
+      expect(store.name()).to(eq('Buckys'))
+    end
+  end
 end
