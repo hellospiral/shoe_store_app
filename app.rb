e8806baea98543e07003678c1949d6aed71b9cf1
Matt Carlson <matthewthomascarlson@gmail.com>
2016-07-22 10:19:26 -0700

    add index, view store, and update store routes to app.rb with passing specs; add views for index, layout, store, and store_edit

--- app.rb
+++ app.rb
@@ -1,3 +1,35 @@
 require("bundler/setup")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @stores = Store.all()
+  erb(:index)
+end
+
+post('/stores') do
+  name = params['name']
+  address = params['address']
+  phone = params['phone']
+  @store = Store.create({name: name, address: address, phone: phone})
+  redirect to('/')
+end
+
+get('/stores/:id') do
+  @store = Store.find(params['id'].to_i)
+  erb(:store)
+end
+
+get('/stores/:id/edit') do
+  @store = Store.find(params['id'].to_i)
+  erb(:store_edit)
+end
+
+patch('/stores/:id') do
+  @store = Store.find(params['id'].to_i)
+  name = params['name']
+  address = params['address']
+  phone = params['phone']
+  @store.update({name: name, address: address, phone: phone})
+  redirect('/stores/' + @store.id.to_s)
+end
---
+++ spec/integration_spec.rb
@@ -0,0 +1,35 @@
+require('spec_helper')
+
+describe('the root path', {:type => :feature}) do
+  it('renders stores and the new store form on the index page') do
+    store = Store.create(name: "Bob's shoes")
+    visit('/')
+    expect(page).to have_content("Bob's shoes")
+  end
+
+  it('creates a new store through the form on the index page') do
+    visit('/')
+    fill_in('name', with: "Bob's shoes")
+    click_button('Add Store')
+    expect(page).to have_content("Bob's shoes")
+  end
+end
+
+describe('the store path', {:type => :feature}) do
+  it('renders store name, address, and phone number on store page') do
+    store = Store.create(name: "Bob's shoes", address: '1234 Main', phone: '555-555-5555')
+    visit('/stores/' + store.id.to_s)
+    expect(page).to have_content("Bob's shoes")
+    expect(page).to have_content('1234 Main')
+    expect(page).to have_content('555-555-5555')
+  end
+
+  it('updates the information for a store') do
+    store = Store.create(name: "Bob's shoes", address: '1234 Main', phone: '555-555-5555')
+    visit('/stores/' + store.id.to_s)
+    click_link('Edit Store')
+    fill_in('Address:', with: '4321 Main St')
+    click_button('Update Store')
+    expect(page).to have_content('4321 Main St')
+  end
+end
